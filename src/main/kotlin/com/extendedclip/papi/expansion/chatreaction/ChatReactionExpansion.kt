b42bd0e6c4f24427994ed29f944617054b864fba
Kqliber <Dylansaidler03@gmail.com>
2020-08-04 22:21:11 +0100

    converted to kotlin (for fun) using the least amount of lines possible

--- src/main/kotlin/com/extendedclip/papi/expansion/chatreaction/ChatReactionExpansion.kt
+++ src/main/kotlin/com/extendedclip/papi/expansion/chatreaction/ChatReactionExpansion.kt
@@ -10,32 +10,22 @@ import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import java.util.*
+import java.util.Calendar
 import kotlin.math.abs
 
 class ChatReactionExpansion : PlaceholderExpansion(), Listener, Cacheable, Configurable {
 
     var getWinner: Player? = null
 
-    override fun getAuthor(): String {
-        return "kaliber"
-    }
+    override fun getAuthor(): String { return "kaliber" }
 
-    override fun getIdentifier(): String {
-        return "chatreaction"
-    }
+    override fun getIdentifier(): String { return "chatreaction" }
 
-    override fun getRequiredPlugin(): String {
-        return "ChatReaction"
-    }
+    override fun getRequiredPlugin(): String { return "ChatReaction" }
 
-    override fun getVersion(): String {
-        return "1.3.0"
-    }
+    override fun getVersion(): String { return "1.3.0" }
 
-    override fun clear() {
-        getWinner = null
-    }
+    override fun clear() { getWinner = null }
 
     override fun getDefaults(): Map<String, Any> {
         val config: MutableMap<String, Any> = HashMap()
@@ -44,51 +34,32 @@ class ChatReactionExpansion : PlaceholderExpansion(), Listener, Cacheable, Confi
     }
 
     @EventHandler
-    fun onReactionWin(event: ReactionWinEvent) {
-        getWinner = event.winner
-    }
+    fun ReactionWinEvent.onReactionWin() { getWinner = winner }
 
     override fun onRequest(p: OfflinePlayer?, identifier: String): String? {
-        if (p == null) {
-            return ""
-        }
+        if (p == null) return ""
 
         val timeNow = Calendar.getInstance().timeInMillis
         val startTime = ReactionAPI.getStartTime()
         val timeDifference = abs(timeNow - startTime) / 1000
         val timeLimit = getInt("time_limit", 30)
-        lateinit var result: Any
 
         when (identifier.toLowerCase()) {
             "wins" -> return ReactionAPI.getWins(p).toString()
-            "type" -> result = if (ReactionAPI.isStarted()) {
+            "type" -> return if (ReactionAPI.isStarted()) {
                 if (ChatReaction.isScrambled()) "scramble" else "reaction"
-            }
-            else {
-                "none"
-            }
+            } else "none"
 
-            "active_round" -> result = ReactionAPI.isStarted()
-            "display_word" -> result = if (ReactionAPI.getDisplayWord() != null) ReactionAPI.getDisplayWord() else " "
-            "reaction_word" -> result = if (ReactionAPI.getReactionWord() != null) ReactionAPI.getReactionWord() else " "
-            "latest_winner" -> result = if (getWinner != null) getWinner!!.name else " "
-            "start_time" -> result = ReactionAPI.getStartTime()
-            "time_in_seconds" -> result = if (!ReactionAPI.isStarted()) {
-                0
-            }
-            else {
-                timeDifference
-            }
+            "active_round" -> return  ReactionAPI.isStarted().toString()
+            "display_word" -> return ReactionAPI.getDisplayWord() ?: ""
+            "reaction_word" -> return ReactionAPI.getReactionWord() ?: ""
+            "latest_winner" -> return getWinner?.name ?: ""
+            "start_time" -> return startTime.toString()
+            "time_in_seconds" -> return if (!ReactionAPI.isStarted()) "0" else timeDifference.toString()
             "time_remaining" -> {
-                result = if (!ReactionAPI.isStarted()) {
-                    0
-                }
-                else {
-                    timeLimit - timeDifference
-                }
+                return if (!ReactionAPI.isStarted()) "0" else (timeLimit - timeDifference).toString()
             }
-            else -> return null
         }
-        return result.toString()
+        return null
     }
 }
